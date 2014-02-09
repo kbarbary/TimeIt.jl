d3d9cca84ef04a9f75f4a879baf570bbbeda88f0
Kyle Barbary <kylebarbary@gmail.com>
2014-02-09 00:20:27 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# TimeIt
+TimeIt
+======
+
+This module exports a `@timeit` macro that works similarly to the `%timeit`
+magic in IPython.
+
+Installation:
+
+    julia> Pkg.clone("git://github.com/kbarbary/TimeIt.jl.git")
+
+Usage:
+
+    julia> using TimeIt
+
+    julia> x = rand(1000); y = rand(1000)
+
+    julia> @timeit x .* y
+
+Differences from Python `timeit`:
+
+* This macro doesn't turn off the garbage collector, whereas the
+  Python version does. I haven't found consistent timings when turing
+  the gc off.
+
+* This macro aims to make the total time between 0.1 and
+  1 seconds, whereas the Python version aims at 0.2 to 2 seconds.
--- src/TimeIt.jl
+++ src/TimeIt.jl
@@ -1,5 +1,52 @@
 module TimeIt
 
-# package code goes here
+export @timeit
 
-end # module
+macro timen(ex, n)
+    quote
+        local t0 = time_ns()
+        for i = 1:$(esc(n))
+            local val = $(esc(ex))
+        end
+        local t1 = time_ns()
+        (t1 - t0) / 1.e9
+    end
+end
+
+macro timeit(ex)
+    quote
+        local val = $(esc(ex))  # Warm up
+        t = zeros(3)
+        
+        # Determine number of loops so that total time > 0.1s.
+        n = 1
+        for i = 0:9
+            n = 10^i
+            t[1] = @timen $(esc(ex)) n
+            if t[1] > 0.1
+                break
+            end
+        end
+
+        # Two more production runs.
+        for i = 2:3
+            t[i] = @timen $(esc(ex)) n
+        end
+        best = minimum(t) / n
+
+        # Format to nano-, micro- or milliseconds.
+        if best < 1e-6
+            best *= 1e9
+            pre = "n"
+        elseif best < 1e-3
+            best *= 1e6
+            pre = "\u00b5"
+        else
+            best *= 1e3
+            pre = "m"
+        end
+        @printf "%d loops, best of 3: %4.2f %ss per loop\n" n best pre
+    end
+end
+
+end  # module
